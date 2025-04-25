f73321bdac2feb43f26c78e4fbcae82cd560495c
Vladyslav Davydenko <vladyslav@hey.com>
2025-04-25 22:20:41 +0300

    Fix tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
+require "capybara-playwright-driver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :playwright, screen_size: [ 1400, 1400 ]
 end
